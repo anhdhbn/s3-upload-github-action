03ca26ca68f4d16b2f469075f8da74177232ad42
AnhDH <anhdhbn@gmail.com>
2021-07-23 10:54:54 +0000

    - init

---
+++ entrypoint.sh
@@ -0,0 +1,15 @@
+#!/bin/sh 
+
+set -e
+
+mkdir -p ~/.aws
+
+touch ~/.aws/credentials
+
+echo "[default]
+aws_access_key_id = ${AWS_ACCESS_KEY_ID}
+aws_secret_access_key = ${AWS_SECRET_ACCESS_KEY}" > ~/.aws/credentials
+
+aws s3 cp ${FILE} s3://${S3_BUCKET}/
+
+rm -rf ~/.aws
